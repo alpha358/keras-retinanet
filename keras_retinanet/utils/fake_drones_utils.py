4a9363fa96970a280c07b6b03aa3b717207739bc
Alfonsas Juršėnas <alpha358@gmail.com>
2019-03-07 23:38:17 +0200

    minor fix

--- keras_retinanet/utils/fake_drones_utils.py
+++ keras_retinanet/utils/fake_drones_utils.py
@@ -210,7 +210,7 @@ def generate_csv_dataset(
             img = generator.load_image(n)
             img_name = str(n)+'.jpg'
             # save img
-            cv2.imwrite(os.path.join(img_dir, img_name), img)
+            cv2.imwrite(os.path.join(img_dir, img_name), img*255)
 
             # Load GT annotations
             annotations = generator.load_annotations(n)['bboxes']
