b86fd05075b39a20ab78e6656c7009852c5e5f86
Alfonsas Juršėnas <alpha358@gmail.com>
2019-03-22 16:02:56 +0200

    update

--- keras_retinanet/preprocessing/csv_generator.py
+++ keras_retinanet/preprocessing/csv_generator.py
@@ -177,7 +177,6 @@ class CSVGenerator(Generator):
             raise_from(ValueError('invalid CSV annotations file: {}: {}'.format(csv_data_file, e)), None)
         self.image_names = list(self.image_data.keys())
 
-        #TODO: drop non-existing images
         self.drop_nonexisting_images()
 
         super(CSVGenerator, self).__init__(**kwargs)
