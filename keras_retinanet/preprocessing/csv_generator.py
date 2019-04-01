39a8356ddc4ec4c8fee8669b7a9ccedab4ae7810
Alfonsas Juršėnas <alpha358@gmail.com>
2019-04-01 14:35:14 +0300

    fix

--- keras_retinanet/preprocessing/csv_generator.py
+++ keras_retinanet/preprocessing/csv_generator.py
@@ -191,13 +191,14 @@ class CSVGenerator(Generator):
         #   due to stop of the for-loop when deleting elements
         while True:
             try:
-                idx, im_name = next(e)
+                _, im_name = next(e)
                 if not os.path.isfile( os.path.join(self.base_dir, im_name) ):
                     # img not found - removing that line
                     print('Img not found, droping: ', im_name)
                     del self.image_data[im_name]
                     # self.image_data.pop(im_name)
                     del self.image_names[idx]
+                    idx += 1  # img index
             except:
                 break
 
