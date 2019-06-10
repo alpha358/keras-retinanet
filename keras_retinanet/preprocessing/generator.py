7f4e0a5f73ca9672c13c8e177022c52f05a2775c
Alfonsas Juršėnas <alpha358@gmail.com>
2019-06-10 13:33:37 +0300

    minor fix

--- keras_retinanet/preprocessing/generator.py
+++ keras_retinanet/preprocessing/generator.py
@@ -476,7 +476,7 @@ class Generator(keras.utils.Sequence):
 
         # ------------------------------ get bbox stats ------------------------------ #
         missed_box_count,\
-             missed_box_overlaps = compute_missing_bbox_stats(self.anchor_params, self, n_max = 100, assign_missed=assign_missed)
+             missed_box_overlaps = compute_missing_bbox_stats(self.anchor_params, self, n_max = n_max, assign_missed=assign_missed)
 
         overlaps = np.array(missed_box_overlaps).flatten()
 
